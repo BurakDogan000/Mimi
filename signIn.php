bb547a7aa3692b5c39777e0703ef412a3a760346
BurakDogan000 <burak420793@gmail.com>
2023-09-28 22:06:05 +0300

    signIn.css and signIn.php have been edited

--- signIn.php
+++ signIn.php
@@ -3,10 +3,11 @@
 <head>
     <title>Mimi - Explore The World!</title>
     <link href="assets/styles/signIn.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 </head>
 <body>
     <?php include "components/theme/signIn/signIn.html" ?>
     <?php include "components/tools/script.html" ?>
-    <script src="assets/scripts/signIn.js"> </script>
+    <script src="assets/scripts/signIn.js"></script>
 </body>
 </html>
