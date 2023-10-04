5f6acef81a819c69d1244048eb06b8364b2ce388
BurakDogan000 <burak420793@gmail.com>
2023-10-05 01:19:39 +0300

    Authors.html

--- authors.php
+++ authors.php
@@ -3,21 +3,18 @@
 <head>
     <title>Mimi - Explore The World!</title>
     <?php include "components/tools/head.html" ?>
-    <link href="assets/styles/index.css" rel="stylesheet" />
-    <link href="assets/styles/global.css" rel="stylesheet" />
+    <link href="assets/styles/authors.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 </head>
 <body>
     <?php include "components/tools/header.html" ?>
     <?php include "components/tools/navbar.html" ?>
-
     <div class="container-fluid main-section">
-        <?php include "components/theme/categories/categories.html" ?>
+        <div class="container d-flex flex-wrap justify-content-center">
+            <?php include "components/theme/authors/authors.html" ?>
+        </div>
     </div>
-    <?php include "components/tools/subscribe.html" ?>
     <?php include "components/tools/footer.html" ?>
     <?php include "components/tools/script.html" ?>
-    
-    <script src="assets/scripts/index.js"> </script>
 </body>
 </html>
